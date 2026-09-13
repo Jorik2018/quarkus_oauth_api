230dc10c5d3475bd5e43211a7fa25cb7434e02b9
Erik Antonio Alarcón Pinedo <C8VMM@ferrovial.com>
2026-09-12 20:07:12 -0500

    x

--- src/main/java/org/isobit/app/controller/RedisController.java
+++ src/main/java/org/isobit/app/controller/RedisController.java
@@ -3,7 +3,7 @@ package org.isobit.app.rest;
 import io.quarkus.redis.datasource.RedisDataSource;
 import io.quarkus.redis.datasource.keys.KeyCommands;
 import io.quarkus.redis.datasource.value.ValueCommands;
-import jakarta.annotation.security.Authenticated;
+import io.quarkus.security.Authenticated;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
